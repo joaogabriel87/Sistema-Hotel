349a361d5bdc4198021076f88d6f98cd7d20dbf5
joaogabriel87 <joagabriel9@gmail.com>
2022-12-31 15:27:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Sistema_de_resrvar_de_um_hotel.Models;
+
+
+
+
+
+
+ List<Pessoa> hospedes = new List<Pessoa>();
+
+ Pessoa p1 = new Pessoa (nome: "Hospede 1");
+ Pessoa p2 = new Pessoa (nome: "Hospede 2");
+
+
+ hospedes.Add(p1);
+ hospedes.Add(p2);
+ 
+
+ Suite suite = new Suite (tiposuite: "Premium", capacidade: 2, valordiaria: 50M);
+
+ Reserva reserva = new Reserva(diasReservados: 10);
+ reserva.CadastrarSuite(suite);
+ reserva.CadastrarHospedes(hospedes);
+
+ Console.WriteLine($"Hospede: {reserva.ObterQuantidadeHospedes()}");
+ Console.WriteLine($"Hospede: {reserva.CalcularValorDiaria()}");
---
+++ README.md
@@ -0,0 +1,2 @@
+Sistema para hotel utilizando C#
+Realizando a reserva e ao final mostrar o valor total *Se os dias de reserva passar de 10 dias, o hospede recebe 10% de desconto
